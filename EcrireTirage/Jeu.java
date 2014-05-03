ba39406f3af0d5d23c434dd9abe70d4db29b3a68
Remi <remi@remi-Satellite-L655.(none)>
2014-05-03 17:29:45 +0200

    Fin de l ecriture des tirages

---
+++ EcrireTirage/Jeu.java
@@ -0,0 +1,77 @@
+import java.util.Random;
+import java.io.*;
+/**
+ * classe jeu modelise des
+ * tirages de triomino
+ */
+public class Jeu{
+    // attributs
+    private int largeur;
+    private Triomino[] tab=null;
+    /**
+     * constructeur general
+     * @param largeur
+     * largeur du plateau destine a recevoir les triominos
+     * @param base
+     * nombre de valeurs possibles par face avant de la tirer au sort (2 si binaire, 16 si hexa)
+     */
+    Jeu(int largeur,int base) {
+	this.largeur=largeur;
+	tab=new Triomino[largeur*largeur];
+	Random generator = new Random();
+	for (int i=0;i<largeur*largeur;i++) {
+	    tab[i]=new Triomino(Math.abs(generator.nextInt()%base), Math.abs(generator.nextInt()%base), Math.abs(generator.nextInt()%base));
+	}
+    }
+    /**
+     * methode ecrit
+     * ecrit le tirage de triominos dans un fichier texte
+     */
+    void ecrit(PrintWriter pw) {
+    	String l1 = new String();
+    	String l2 = new String();
+    	String l3 = new String();
+		for(int i=0; i<largeur*largeur;i++)
+			l1 += "   ^  ";
+		pw.println(l1);
+		for(int i=0; i<largeur*largeur;i++)
+			l2 += "  "+Triominos.onechar(tab[i].c) + " "+Triominos.onechar(tab[i].b)+ " ";
+		pw.println(l2);
+		for(int i=0; i<largeur*largeur;i++)
+			l3 += " /_" + Triominos.onechar(tab[i].a) + "_\\";
+		pw.println(l3);
+    }
+    
+    /**
+     * methode affiche
+     * affiche le tirage sous forme textuelle
+     * affichage des triominos cote a cote sur les 3 memes lignes
+     * retour a la ligne tous les 12 triominos
+     */
+    void affiche() {
+      for(int i=0; i<largeur*largeur;i+=12){
+        int j1,j2,j3;
+        for(j1 = i ; (j1 < largeur*largeur) && (j1 < i+12) ; j1++)
+          System.out.print("   ^  ");
+        System.out.println("");
+        for(j2 = i ; (j2 < largeur*largeur) && (j2 < i+12) ; j2++)
+          System.out.print("  "+Triominos.onechar(tab[j2].c) + " "+Triominos.onechar(tab[j2].b)+ " ");
+        System.out.println("");
+        for(j3 = i ; (j3 < largeur*largeur) && (j3 < i+12) ; j3++)
+          System.out.print(" /_" + Triominos.onechar(tab[j3].a) + "_\\");
+        System.out.println("");
+      }
+    }
+    /**
+     * methode d acces au triomino d un tirage
+     * @param i
+     * rang du triomino dans le tirage
+     * @return
+     * triomino
+     */
+    Triomino get(int i) {
+	return tab[i];
+    }
+
+
+}
---
+++ EcrireTirage/Triomino.java
@@ -0,0 +1,24 @@
+/**
+* classe Triomino modelise le triomino
+* en fonction de la valeur inscrite
+* sur chacune de ses trois faces
+*/
+public class Triomino{
+// attributs
+    int a,b,c;
+/**
+* constructeur general
+* @param a
+* 			valeur de la face 1
+* @param b
+* 			valeur de la face 2
+* @param c
+* 			valeur de la face 3
+*/
+    Triomino(int a,int b,int c) {
+	this.a=a;
+	this.b=b;
+	this.c=c;
+    }
+}
+
